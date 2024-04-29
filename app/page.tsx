d6f3d678f0097c275dad5c9c1b0d3556a6706e21
Stanley Nzegwu <nzegwustanley@gmail.com>
2024-04-29 12:07:07 +0100

    new commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,10 @@ import Header from "@/components/shared/Header";
 
 export default function Home() {
   return (
-    <main className="p-2 md:p-4 lg:p-6 w-full h-screen">
+    <main className="w-full h-screen">
+      {/* absolutely positioned divs */}
+      <div className="absolute md:hidden lg:inline left-0 top-0 w-60 h-40 -translate-y-1/2 -translate-x-1/4 rounded-b-full bg-purple-900 blur-3xl overflow-hidden z-0" />
+      <div className="absolute right-0 top-0 -translate-y-1/2 w-40 h-40 md:w-56 md:h-48  rounded-l-full bg-violet-900 blur-3xl overflow-hidden z-0" />
       <Header />
     </main>
   );
--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -60,9 +60,16 @@ import Image from "next/image";
 
 const Header = () => {
   return (
-    <header className="flex flex-col justify-center w-full h-full py-6 ">
+    <header className="relative flex flex-col justify-center w-full h-full py-6 z-50 ">
+      {/* absolutely positioned divs */}
+      {/* <div className="absolute md:hidden lg:inline left-0 top-0 w-60 h-40 -translate-y-1/2 -translate-x-1/4 rounded-b-full bg-purple-900 blur-3xl overflow-hidden z-0" />
+      <div className="absolute right-0 top-0  w-40 h-40 md:w-56 md:h-48  rounded-l-full bg-violet-900  overflow-hidden z-0" /> */}
+      {/* <div className="absolute left-0 w-40 h-40 -translate-y-1/2 -translate-x-1/3 rounded-b-full bg-purple-900 blur-3xl overflow-hidden z-0" />
+  <div className="absolute right-0 w-40 h-40 md:w-56 md:h-48 -translate-y-1/2 translate-x-1/3 rounded-l-full bg-violet-900 blur-3xl overflow-hidden z-0" />
+  <div className="absolute hidden md:block left-32 bottom-20 w-56 h-40  -translate-y-1/2  rounded-full bg-violet-900  overflow-hidden blur-4xl z-10" />
+  <div className="absolute hidden md:block left-20 bottom-10 w-56 h-40  -translate-y-1/2  rounded-full bg-blue-900  overflow-hidden blur-4xl z-0" /> */}
       <div className=" flex flex-col lg:flex-row lg:justify-center items-center lg:gap-20 ">
-        <div className=" flex flex-col items-center max-md:mr-20">
+        <div className=" flex flex-col items-center max-lg:mr-60 max-md:mr-20">
           <div className="">
             <h1 className="capitalize text-4xl md:text-6xl lg:text-7xl">Ecosystem</h1>
             <h2 className="capitalize text-sm md:text-xl font-medium text-purple-200">
@@ -70,10 +77,28 @@ const Header = () => {
             </h2>
           </div>
         </div>
-        <div className=" flex flex-col items-center max-md:ml-14 self-end">
+        <div className="relative flex flex-col items-center max-md:ml-14 md:self-end">
           <Image
             className="w-72 h-72 sm:w-80 sm:h-80 md:w-96 md:h-96 lg:w-96 lg:h-96 object-contain"
             src={"/images/Visual.svg"}
+            alt="visual"
+            width={500}
+            height={500}
+            loading="lazy"
+          />
+
+          {/* Pyramid And Torus Image */}
+          <Image
+            className="bottom-0 left-0 -translate-x-14 md:-translate-x-20 absolute w-20 h-20 md:w-28 md:h-28 object-contain"
+            src={"/images/pyramid.svg"}
+            alt="visual"
+            width={500}
+            height={500}
+            loading="lazy"
+          />
+          <Image
+            className="top-0 right-0 md:left-0 -translate-y-32 md:translate-x-1/4 lg:-translate-y-20 absolute w-20 h-20 md:w-28 md:h-28 object-contain"
+            src={"/images/torus.svg"}
             alt="visual"
             width={500}
             height={500}
